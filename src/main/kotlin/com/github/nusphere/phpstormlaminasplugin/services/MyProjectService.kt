b07b545bdedc446b2f76b230df85993545a17867
GitHub Action <action@github.com>
2022-09-26 19:27:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/nusphere/phpstormlaminasplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nusphere/phpstormlaminasplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.nusphere.phpstormlaminasplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nusphere.phpstormlaminasplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
